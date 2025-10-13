f99f7d1b45837dcabba6c40588a81dba20f777c4
Dmitriy Shilin <dshil@fastmail.com>
2025-10-13 08:33:17 +0200

    Formatting

--- src/lib/algo/time_ops.ts
+++ src/lib/algo/time_ops.ts
@@ -5,8 +5,14 @@ export class TimeOps {
     const minutes = Math.floor((seconds % 3600) / 60);
     const secs = seconds % 60;
 
-    if (hours > 0) return `${hours}h ${minutes}m`;
-    if (minutes > 0) return `${minutes}m ${secs}s`;
+    if (hours > 0) {
+      return `${hours}h ${minutes}m`;
+    }
+
+    if (minutes > 0) {
+      return `${minutes}m ${secs}s`;
+    }
+
     return `${secs}s`;
   }
 }
